8b8cda88dd5065b2a70354e2e53d8d56650b571b
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-09-21 11:56:01 -0300

    feature: header styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,22 @@
+import Nav from "./Nav";
+import { Container } from "./styles";
+import IconAltatech from "../../assets/Altatech_icon_white.svg";
+import Search from "../Search";
+
 function Header() {
-  return <h1>Header</h1>;
+  return (
+    <Container>
+      <ul>
+        <img src={IconAltatech} alt="Altatech Icon" />
+        <div className="header">
+          <Search />
+          <Nav title="Serviços" path="#services" />
+          <Nav title="Produtos" path="#products" />
+          <Nav title="Contato" path="#contact" />
+        </div>
+      </ul>
+    </Container>
+  );
 }
 
 export default Header;
