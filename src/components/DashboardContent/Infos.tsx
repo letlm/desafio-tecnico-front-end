03177c0faeb3581f73870515b2163037a194fab6
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-09-25 12:38:25 -0300

    feature: inserting hover on buttons

--- src/components/DashboardContent/Infos.tsx
+++ src/components/DashboardContent/Infos.tsx
@@ -16,7 +16,23 @@ function Infos({ title, subtitle, img, more }: IInfos) {
 
           <span className="subtitle">{subtitle}</span>
         </div>
-        <button>{more}</button>
+        <button>
+          <span className="btnText">{more}</span>
+          <span className="icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+            >
+              <path fill="none" d="M0 0h24v24H0z"></path>
+              <path
+                fill="currentColor"
+                d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+              ></path>
+            </svg>
+          </span>
+        </button>
       </div>
     </ContextInfos>
   );
